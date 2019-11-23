8faa026abe86635c20741bd586de773c7e57d56c
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-11-23 17:20:12 +0900

    railsとdeviseを日本語化

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ Bundler.require(*Rails.groups)
 module Bboard
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.default_locale = :ja
     config.load_defaults 5.2
     config.generators.template_engine = :slim
     config.generators do |g|

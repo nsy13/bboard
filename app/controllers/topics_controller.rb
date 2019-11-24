f687816464f20773fbe12af9c59330476918b9fe
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-11-24 13:40:32 +0900

    スレッド(topic)作成機能実装

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,39 @@
 class TopicsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    byebug
+    @topic = current_user.topics.build(topic_params)
+    if @topic.save
+      flash[:success] = "スレッドを作成しました"
+      redirect_to @topic
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:name)
+  end
 end

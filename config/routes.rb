b46ad02cf4c1bc81943260f7db5093cae0795c6e
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-11-24 23:07:01 +0900

    headerを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users
+  devise_scope :user do
+    get 'signup', to: 'users/registrations#new'
+    post 'signup', to: 'users/registrations#create'
+    delete 'delete', to: 'users/registrations#destroy'
+    get 'login', to: 'devise/sessions#new'
+    post 'login', to: 'devise/sessions#create'
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
   resources :users, only: :show
   resources :topics
 end

518bc785cb5617565e4b779d64c0eb1e508495cc
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-11-24 22:33:18 +0900

    ダミーデータ追加とスレッド一覧UIの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # テストログイン用ユーザー
 
-User.create!(name: "テスト太郎",
+User.create!(name: "Test-Tarou",
              email: "test@example.com",
              password: "password")
 user = User.first
@@ -12,7 +12,9 @@ end
 # その他ユーザー
 
 30.times do |n|
-  User.create!(name: Faker::Name.name,
+  User.create!(name: Faker::Internet.username,
                email: "no.#{n + 1}-#{Faker::Internet.email}",
                password: "password")
+  user = User.find(n + 1)
+  user.topics.create!(name: "#{Faker::Music.band}総合スレ")
 end

8cb0185b19937c9570e4d1663d6fba4fffafdad9
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-11-24 12:55:18 +0900

    Topicモデルを生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_22_191751) do
+ActiveRecord::Schema.define(version: 2019_11_24_034910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_topics_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +36,5 @@ ActiveRecord::Schema.define(version: 2019_11_22_191751) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "topics", "users"
 end

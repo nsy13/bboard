91bf534be07de3dd4e3ac826cfae589bf518e628
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-11-25 22:46:01 +0900

    コメントの編集と削除機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,30 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    @topic = @post.topic
+  end
+
+  def update
+    post = Post.find(params[:id])
+    topic = post.topic
+    if post.update_attributes(post_params)
+      flash[:success] = "コメントを更新しました"
+      redirect_to topic_path(topic)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    topic = post.topic
+    post.delete
+    flash[:success] = "コメントを削除しました"
+    redirect_to topic_path(topic)
+  end
+
   private
 
   def post_params

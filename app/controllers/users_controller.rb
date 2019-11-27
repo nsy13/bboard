6d9555922c6d63313af1ec4916bcd54a11c5f4cd
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-11-27 22:54:09 +0900

    ユーザーマイページ表示の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    # @threads
+    @topics = Topic.order(:updated_at).page(params[:page]).per(TOPICS_NUMBER)
   end
 end

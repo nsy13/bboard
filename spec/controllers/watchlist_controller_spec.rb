3d3086fe5aa0ae24521289a19fdb3528447df466
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-11-27 22:19:12 +0900

    テスト追加

--- spec/controllers/watchlist_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe WatchlistController, type: :controller do
-
-end
--- spec/factories/watchlists.rb
+++ spec/factories/watchlists.rb
@@ -1,6 +1,6 @@
-FactoryBot.define do
-  factory :watchlist do
-    user { nil }
-    topic { nil }
-  end
-end
+# FactoryBot.define do
+#   factory :watchlist do
+#     associate :user
+#     associate :topic
+#   end
+# end

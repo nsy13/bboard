5f730b274544996eafe19d2cbff17037c9331729
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-11-28 03:11:13 +0900

    親テーブルにdependentオプションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :topics
-  has_many :posts
-  has_many :watchlists
+  has_many :topics, dependent: :destroy
+  has_many :posts, dependent: :destroy
+  has_many :watchlists, dependent: :destroy
   has_many :topics, through: :watchlists
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable

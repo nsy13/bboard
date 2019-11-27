d570679970a8097269b2ba8105e7684edf5c2f02
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-11-27 16:41:42 +0900

    categoriesテーブルのnameカラムに一意性を追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   has_many :topic_categories
   has_many :topics, through: :topic_categories
-  validates :name, presence: true, length: { maximum: 30 }
+  validates :name, presence: true, length: { maximum: 30 }, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_031404) do
+ActiveRecord::Schema.define(version: 2019_11_27_074001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_11_26_031404) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "posts", force: :cascade do |t|

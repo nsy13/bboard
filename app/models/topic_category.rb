fae2e4a65f98e8be923819e7ae4ccfac2655cb82
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2019-11-28 16:19:52 +0900

    topic_categoryに複合外部キーインデックスを追加

--- app/models/topic_category.rb
+++ app/models/topic_category.rb
@@ -1,4 +1,5 @@
 class TopicCategory < ApplicationRecord
   belongs_to :category
   belongs_to :topic
+  validates :topic_id, uniqueness: { scope: :category_id }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_185558) do
+ActiveRecord::Schema.define(version: 2019_11_28_070419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_185558) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_topic_categories_on_category_id"
+    t.index ["topic_id", "category_id"], name: "index_topic_categories_on_topic_id_and_category_id", unique: true
     t.index ["topic_id"], name: "index_topic_categories_on_topic_id"
   end
 

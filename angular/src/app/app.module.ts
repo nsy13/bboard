6855ee5762f4671242b601d80773bf6580cefaab
Shuhei Yoshino <s.yoshino.1113@gmail.com>
2020-01-05 14:54:03 +0900

    各種コンポーネントを生成

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { TopicsComponent } from './topics/topics.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CategoriesComponent,
+    TopicsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
